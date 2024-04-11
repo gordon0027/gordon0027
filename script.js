d66f69e7cb0cd3fef1294fe8e0b548f5aa47dc21
gordon0027 <149299552+gordon0027@users.noreply.github.com>
2024-04-11 09:49:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const themeButton = document.getElementById('themeButton');
 const body = document.body;
-const header = document.getElementByClass("header-div");
+const header = document.header;
+// const header = document.getElementByClass("header-div");
 
 themeButton.addEventListener('click', () => {
   body.classList.toggle('dark-theme')
