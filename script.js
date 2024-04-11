3450c15085eddedc66a91ce3cba416ca5954913b
gordon0027 <149299552+gordon0027@users.noreply.github.com>
2024-04-11 09:46:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const themeButton = document.getElementById('themeButton');
 const body = document.body;
+const header = document.getElementByClass("header-div");
 
 themeButton.addEventListener('click', () => {
   body.classList.toggle('dark-theme')
+  header.classList.toggle('dark-theme')
 });
